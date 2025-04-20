5cfd1ea677a76c48d7d97a2331a9a8afca533925
elektra caffrey <ecaffrey@unm.edu>
2025-04-19 18:49:10 -0600

    messing with parameters

--- mlhw4.py
+++ mlhw4.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader, TensorDataset
 
 torch.set_num_threads(1)
 #Task 1
-df = pd.read_csv('movie_data.csv')
+df = pd.read_csv('C:\\Users\\Elekt\\spring25\\cs529\\mlhw4\\movie_data.csv')
 print(df.head())
 print(df.shape)
 
@@ -64,34 +64,56 @@ class FNN(torch.nn.Module):
         x = torch.relu(self.fc1(x))
         return self.fc2(x) 
 
-# Initialize
-model = FNN(X_train_tensor.shape[1])
-learning_rate = 0.01
-loss_fn = torch.nn.CrossEntropyLoss()
-
-# Training with gradient update
-num_epochs = 5
-for epoch in range(num_epochs):
-    for xb, yb in train_loader:
-        pred = model(xb)
-        loss = loss_fn(pred, yb)
-        loss.backward()
-
-        with torch.no_grad():
-            for param in model.parameters():
-                param -= learning_rate * param.grad
-                param.grad.zero_()
-
-    print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
-
-
 # softmax function (from notes)
 def softmax(z):
     return torch.exp(z) / torch.sum(torch.exp(z), dim=1, keepdim=True)
 
-with torch.no_grad():
-    logits = model(X_test_tensor)
-    probs = softmax(logits)
-    predicted_labels = torch.argmax(probs, dim=1)
-    acc = (predicted_labels == y_test_tensor).float().mean()
-    print(f"Test Accuracy: {acc:.4f}")
+# Initialize
+#model = FNN(X_train_tensor.shape[1])
+learning_rates = [0.1, 0.01, 0.001]
+weight_decays = [1e-4, 1e-5, 0]
+#optimizer = torch.optim.Adam(model.parameters(), learning_rate)
+#loss_fn = torch.nn.CrossEntropyLoss()
+best_acc = 0
+best_params = {}
+
+for lr in learning_rates:
+    for wd in weight_decays:
+        print(f"\nTesting lr={lr}, weight_decay={wd}")
+        model = FNN(X_train_tensor.shape[1])
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+        loss_fn = torch.nn.CrossEntropyLoss()
+        # Training with gradient update
+        num_epochs = 5
+        for epoch in range(num_epochs):
+            for xb, yb in train_loader:
+                # reset gradients
+                optimizer.zero_grad()
+                pred = model(xb)
+                loss = loss_fn(pred, yb)
+                loss.backward()
+                #update model weights
+                optimizer.step()
+
+                with torch.no_grad():
+                    for param in model.parameters():
+                        param -= lr * param.grad
+                        param.grad.zero_()
+
+            print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
+
+
+
+        with torch.no_grad():
+            logits = model(X_test_tensor)
+            probs = softmax(logits)
+            predicted_labels = torch.argmax(probs, dim=1)
+            acc = (predicted_labels == y_test_tensor).float().mean()
+            print(f"Test Accuracy: {acc:.4f}")
+
+            if acc > best_acc:
+                best_acc = acc
+                best_params = {'lr':lr, 'wd': wd}
+
+print(f"\nBest accuracy: {best_acc:.4f}")
+print(f"Best parameters: {best_params}")
